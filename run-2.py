5d9690d6ab8424f23886e73fe417d0a84f03b852
Murilo Nham Oliveira De Castro <mnodcastro@paschoalotto.com.br>
2020-10-20 11:19:14 -0300

    linha print removida

--- run-2.py
+++ run-2.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 @app.route('/', methods = ['POST'])
 def chamar():
     Response = requests.post(url="http://0.0.0.0:5020/")
-    print(Response)
-    return None
+    return Response
 
 if __name__== '__main__':
     app.run(host='0.0.0.0', port=5021)
