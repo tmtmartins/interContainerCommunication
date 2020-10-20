eea806c12bb794556814b4863c1e7b2f6e095383
Murilo Nham Oliveira De Castro <mnodcastro@paschoalotto.com.br>
2020-10-20 16:05:52 -0300

    volta do Response

--- run-1.py
+++ run-1.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import Response
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def teste():
-    return ('FUNCIONANDO')
+    return Response("{'message':'FUNCIONANDO'}", status=200, mimetype='application/json')
 
 if __name__== '__main__':
     app.run(host='0.0.0.0', port=5020)
--- run-2.py
+++ run-2.py
@@ -1,11 +1,15 @@
 from flask import Flask
-from flask import redirect
+from flask import Response
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def chamar():
-    return redirect(api-teste1, code=302)
+    response = requests.POST(api-teste1)
+    print(response)
+    return(response)
+    #return redirect(api-teste1, code=302)
 
 if __name__== '__main__':
     app.run(host='0.0.0.0', port=5021)
