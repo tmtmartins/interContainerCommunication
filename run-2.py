45e4bb8afc675a1353062454cd0184169fa83532
Murilo Nham Oliveira De Castro <mnodcastro@paschoalotto.com.br>
2020-10-20 11:12:04 -0300

    teste return None

--- run-2.py
+++ run-2.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['POST'])
 def chamar():
-    response = requests.post(url="localhost:5020/")
-    print(response)
-    return response
+    Response = requests.post(url="http://0.0.0.0:5020/")
+    print(Response)
+    return None
 
 if __name__== '__main__':
     app.run(host='0.0.0.0', port=5021)
