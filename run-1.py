4a2d3b41444678eb02e9ce35f73018502a7ca2e7
Murilo Nham Oliveira De Castro <mnodcastro@paschoalotto.com.br>
2020-10-20 14:58:37 -0300

    teste

--- run-1.py
+++ run-1.py
@@ -3,7 +3,7 @@ from flask import Response
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/teste', methods=['GET'])
 def teste():
     return Response("{'message':'TESTE'}", status=200, mimetype='application/json')
 
