e3e1f19daa459578b0d858d3d6094c74a1c19355
Murilo Nham Oliveira De Castro <mnodcastro@paschoalotto.com.br>
2020-10-20 14:40:40 -0300

    teste route 127

--- run-2.py
+++ run-2.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def chamar():
     #response = request.post(url="http://0.0.0.0:5020/")
     #print(response)
-    return redirect("http://0.0.0.0:5020/", 'teste-api')
+    return redirect("http://127.0.0.1:5020/", 'teste-api')
 
 if __name__== '__main__':
     app.run(host='0.0.0.0', port=5021)
