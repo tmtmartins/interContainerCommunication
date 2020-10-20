dd4ce1aba927d19eb6fd1b0a5f436ccbc26db3f1
Murilo Nham Oliveira De Castro <mnodcastro@paschoalotto.com.br>
2020-10-20 14:51:56 -0300

    teste com localhost

--- run-2.py
+++ run-2.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 def chamar():
     #response = request.post(url="http://0.0.0.0:5020/")
     #print(response)
-    return redirect("http://127.0.0.1:5020/", code=302)
+    #return redirect("http://0.0.0.0:5020/", code=302)
+    return redirect("localhost:5020/", code=302)
 
 if __name__== '__main__':
     app.run(host='0.0.0.0', port=5021)
