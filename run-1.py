da0c2ff471d87955ae5b2fb593e51763617ad9b1
Murilo Nham Oliveira De Castro <mnodcastro@paschoalotto.com.br>
2020-10-20 15:15:44 -0300

    teste sem response

--- run-1.py
+++ run-1.py
@@ -1,11 +1,10 @@
 from flask import Flask
-from flask import Response
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def teste():
-    return Response("{'message':'TESTE'}", status=200, mimetype='application/json')
+    return ('teste')
 
 if __name__== '__main__':
     app.run(host='0.0.0.0', port=5020)
--- run-2.py
+++ run-2.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import Response, redirect
+from flask import redirect
 
 app = Flask(__name__)
 
@@ -7,8 +7,8 @@ app = Flask(__name__)
 def chamar():
     #response = request.post(url="http://0.0.0.0:5020/")
     #print(response)
-    #return redirect("http://0.0.0.0:5020/", code=302)
-    return redirect("http://172.22.0.1:5020/", code=302)
+    return redirect("http://0.0.0.0:5020/", code=302)
+    #return redirect("http://172.22.0.1:5020/", code=302)
     #return redirect("http://10.50.50.100:5011/", code=302) -- teste recuperação de ligação Otto
 
 if __name__== '__main__':
