25c6aa07cac5f4ec45b54b26010ec1771d543abd
Murilo Nham Oliveira De Castro <mnodcastro@paschoalotto.com.br>
2020-10-20 15:08:33 -0300

    post add on run-1

--- run-2.py
+++ run-2.py
@@ -8,7 +8,7 @@ def chamar():
     #response = request.post(url="http://0.0.0.0:5020/")
     #print(response)
     #return redirect("http://0.0.0.0:5020/", code=302)
-    return redirect("http://172.22.0.1:5020/teste", code=302)
+    return redirect("http://172.22.0.1:5020/", code=302)
     #return redirect("http://10.50.50.100:5011/", code=302) -- teste recuperação de ligação Otto
 
 if __name__== '__main__':
