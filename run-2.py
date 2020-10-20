f6594605006020b00b49d1a4720b6bb378765721
Murilo Nham Oliveira De Castro <mnodcastro@paschoalotto.com.br>
2020-10-20 16:25:57 -0300

    teste nome

--- run-2.py
+++ run-2.py
@@ -9,7 +9,6 @@ def chamar():
     response = request(api-teste1)
     print(response)
     return(response)
-    #return redirect(api-teste1, code=302)
 
 if __name__== '__main__':
     app.run(host='0.0.0.0', port=5021)
