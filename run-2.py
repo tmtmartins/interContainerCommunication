4a983678d13b758b7a3d3033264073126d2b76fc
Murilo Nham Oliveira De Castro <mnodcastro@paschoalotto.com.br>
2020-10-20 15:53:19 -0300

    teste com <br>

--- run-2.py
+++ run-2.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def chamar():
-    return redirect('https://api-teste1', code=302)
+    return redirect(api-teste1, code=302)
 
 if __name__== '__main__':
     app.run(host='0.0.0.0', port=5021)
