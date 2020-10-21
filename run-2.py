5b421aaf6cc5ff5175ee91d8613afe1edba3a2da
Murilo Nham Oliveira De Castro <mnodcastro@paschoalotto.com.br>
2020-10-21 14:27:18 -0300

    str into return/print

--- run-2.py
+++ run-2.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def chamar():
     response = requests.get('http://api-teste1:5020')
-    print(response)
-    return(response)
+    print str(response)
+    return str(response)
 
 if __name__== '__main__':
     app.run(host='0.0.0.0', port=5021)
