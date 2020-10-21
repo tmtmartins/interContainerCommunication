777e5baac91ae0331c6d9e95356b911959371445
Murilo Nham Oliveira De Castro <mnodcastro@paschoalotto.com.br>
2020-10-21 13:48:12 -0300

    att response

--- run-1.py
+++ run-1.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def teste():
-    return Response("{'message':'FUNCIONANDO'}", status=200, mimetype='application/json')
+    return Response("{'message':'FUNCIONANDO'}")
 
 if __name__== '__main__':
     app.run(host='0.0.0.0', port=5020)
