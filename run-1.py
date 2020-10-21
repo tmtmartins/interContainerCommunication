884ca00d3e8cb95b06a1710bad71815964d51b0f
Murilo Nham Oliveira De Castro <mnodcastro@paschoalotto.com.br>
2020-10-21 13:56:49 -0300

    teste response add

--- run-1.py
+++ run-1.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def teste():
-    return Response("{'message':'FUNCIONANDO'}")
+    return Response('FUNCIONANDO')
 
 if __name__== '__main__':
     app.run(host='0.0.0.0', port=5020)
